7980de6dea1651d713765c7e69911fbae7731c5a
Andrew Sivaprakasam <andrew.n.sivaprakasam@gmail.com>
2023-12-19 15:26:12 -0500

    in the process of checking various types of fit files/determining activity type etc

--- Analysis/R/analyzeFit.R
+++ Analysis/R/analyzeFit.R
@@ -1,4 +1,4 @@
-data_dir <- "/home/sivaprakasaman/Documents/Code/running_analytics/Data/Races/HM";
+data_dir <- "/home/sivaprakasaman/Documents/Code/running_analytics/Data/testSet";
 cwd <- getwd();
 setwd(data_dir);
 
@@ -81,42 +81,60 @@ read_fit <- function(fit_file) {
     bind_rows() %>% 
     arrange(timestamp) 
   
-  distance <- fit_allrecords$distance
-  distance <- distance/1609.34; #to mi
-  distance_shift <- c(0,distance)
+  sport_msg = getMessagesByType(fit,'sport');
+  activity_type = sport_msg[[2]];
+    
+  Distance <- fit_allrecords$distance
+  Distance <- Distance/1609.34; #to mi
+  distance_shift <- c(0,Distance)
   distance_shift <- distance_shift[1:length(distance_shift)-1];
     
   time_absolute <- as.POSIXct(fit_allrecords$timestamp);
   t_vect <- as.numeric(time_absolute);
-  time <- t_vect-min(t_vect);
-  time_shift <- c(0,time);
+  Time <- t_vect-min(t_vect);
+  time_shift <- c(0,Time);
   time_shift <- time_shift[1:length(time_shift)-1];
   
-  dx <- distance-distance_shift; #delta position
-  dt <- time-time_shift;
+  dx <- Distance-distance_shift; #delta position
+  dt <- Time-time_shift;
   
   #in mi/s
-  pace <- dx/dt;
-  pace <- 1/(pace*60);
+  Pace <- dx/dt;
+  Pace <- 1/(Pace*60);
 
   #converting cadence (only needed for non-biking)
-  cadence = (fit_allrecords$cadence+fit_allrecords$fractional_cadence)*2;
+  Cadence = (fit_allrecords$cadence+fit_allrecords$fractional_cadence)*2;
   
   #converting altitude...actually enhanced_altitude is just in meters?? altitude may be something different??
-  elevation = fit_allrecords$enhanced_altitude*3.28084; #to feet
   
+  #if barometer data
+  if("enhanced_altitude" %in% colnames(fit_allrecords)){
+    Elevation = fit_allrecords$enhanced_altitude
+  }else if ("altitude" %in% colnames(fit_allrecords)){
+    Elevation = fit_allrecords$altitude
+  }else{
+    Elevation = matrix(NA, ncol = 1, nrow = nrow(fit_allrecords)) 
+  }
+  
+  Elevation = Elevation*3.28084; #to feet
+    
   #other data (some of which might not be present)
-  data_heads = c("heart_rate","cadence","fractional_cadence","step_length","stance_time_balance","vertical_ratio","stance_time","power")
+  data_heads = c("heart_rate","step_length","stance_time_balance","vertical_ratio","stance_time","power")
+  other_data = search_and_extract_headers(fit_allrecords,data_heads);
   
   # Create a data frame
-  data <- data.frame(Time = time, Distance = distance, Pace = pace, Cadence = cadence, Elevation = elevation, Power = fit_allrecords$power,Lat = fit_allrecords$position_lat, Lon = fit_allrecords$position_long)
-
+  data <- data.frame(distance = Distance, pace = Pace, cadence = Cadence, elevation = Elevation, lat = fit_allrecords$position_lat, lon = fit_allrecords$position_long)
+  data <- cbind(data, other_data)
+    
   # Z-score data, and reject any points > +\- 3SDs above mean (assume measurement noise)
   data <- remove_outliers_df(data)
+  data$time <- Time;
+  data = relocate(data,'time',1);
+
+  elev_gainloss = calculate_elevation_gain_smoothed(Elevation,window_size = 40);
+  meta_data <- data.frame(Activity = activity_type, total_Ascent = elev_gainloss[[1]], total_Ascent = elev_gainloss[[2]]);
   
-  activity_type = fit_allrecords$activity_type[1];
-  
-  meta_data <- data.frame(Activity = activity_type, total_Ascent = ascent, total_Descent = descent);
+  out = list(data,meta_data);
   
   return(out)
   
@@ -125,11 +143,12 @@ read_fit <- function(fit_file) {
 # Function to read either FIT or TCX file
 read_fitness_data <- function(file_path) {
   file_extension <- tools::file_ext(file_path)
-  
   if (tolower(file_extension) == "tcx") {
     return(read_tcx(file_path))
   } else if (tolower(file_extension) == "fit") {
     return(read_fit(file_path))
+  } else if (tolower(file_extension) == "txt") {
+    return(read_tcx(file_path))
   } else {
     stop("Unsupported file type. Only TCX and FIT files are supported.")
   }
@@ -168,29 +187,63 @@ calculate_elevation_gain_smoothed <- function(elevation_plot, window_size = 40) 
   return(list(elevation_gain,elevation_loss, smoothed_elevation))
 }
 
+
+search_and_extract_headers <- function(df, headers_to_search) {
+  # Create a new dataframe with NAs
+  new_df <- data.frame(matrix(NA, ncol = length(headers_to_search), nrow = nrow(df)))
+  colnames(new_df) <- headers_to_search
+  
+  # Loop through the specified headers and fill in the new dataframe
+  for (header in headers_to_search) {
+    if (header %in% colnames(df)) {
+      new_df[, header] <- df[, header]
+    }
+  }
+  
+  return(new_df)
+}
+
 # Example usage
 file_path <- "Marshall_Half_Marathon_2012.tcx"  # Replace with the actual file path
+
 file_path <- "Zionsville_HM_23.fit"  # Replace with the actual file path
+file_path <- "PGH_10M_2016.fit";
+file_path <- "gym_workout.fit";
+# file_path <- "notTCX2.txt";
 
 # Read FIT or TCX file
-fitness_data <- read_fitness_data(file_path)
+tryCatch(
+  expr = {
+    fitness_data <- read_fitness_data(file_path);
+  },
+  error = function(e){ 
+    # (Optional)
+    # Do this if an error is caught...
+    print("ERROR: Can't parse file.")
+  },
+  finally = {
+    print(paste(file_path,' Processing Complete'))
+  }
+)
 
 # Print the extracted data
 print("Fitness Data:")
 print(fitness_data)
 
-# Plot map
-ggplot(fitness_data, aes(x = Lon, y = Lat)) +
-  coord_quickmap() +
-  geom_point(aes(colour = fitness_data$Distance))
+fit_df = fitness_data[[1]];
+fit_meta = fitness_data[[2]];
 
-coords <- cbind(fitness_data$Lon,fitness_data$Lat)
+# Plot map
+ggplot(fit_df, aes(x = lon, y = lat)) +
+  coord_quickmap() +
+  geom_point(aes(colour = fit_df$distance))
+
+coords <- cbind(fit_df$lon,fit_df$lat)
 
 m <- coords %>% 
   leaflet(  ) %>%
   addTiles() %>%
   addPolylines(color = 'red')
-
 m
 
 setwd(cwd)
